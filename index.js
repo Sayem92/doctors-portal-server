faa6890040c3bc332cf566b70680ffeb913ff077
Sayem92 <sayemmiha123@gmail.com>
2022-11-15 23:12:11 +0600

    get appointment options

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const cors = require('cors');
 const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config();
+
+const jwt = require('jsonwebtoken')
+
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -12,15 +16,38 @@ app.use(express.json());
 
 
 
-const uri = "mongodb+srv://<username>:<password>@cluster0.lhckmem.mongodb.net/?retryWrites=true&w=majority";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.lhckmem.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
-  client.close();
-});
 
 
+
+async function run() {
+    try {
+        const appointmentOptionsCollection = client.db("DoctorsPortal").collection("appointmentOptions");
+
+       
+
+
+        app.get('/appointmentOptions', async (req, res) => {
+            const query = {}
+            const options = await appointmentOptionsCollection.find(query).toArray();
+            res.send(options);
+        })
+
+
+
+
+
+
+    }
+    catch (err) {
+        console.log(err);
+    }
+
+
+}
+
+run().catch(err => console.log(err))
 
 
 
