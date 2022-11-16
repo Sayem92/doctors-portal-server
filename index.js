9e14168b0394db4b8dac030ba5e34b2a406755e0
Sayem92 <sayemmiha123@gmail.com>
2022-11-16 13:04:15 +0600

    add booking conditional

--- index.js
+++ index.js
@@ -95,11 +95,26 @@ async function run() {
             res.send(options);
 
         });
-        
+
 
         // post bookings-------
         app.post('/bookings', async (req, res) => {
             const booking = req.body;
+            // console.log(booking);
+            const query = {
+                appointmentDate: booking.appointmentDate,
+                email : booking.email,
+                treatment : booking.treatment
+
+            }
+
+            const alreadyBooked = await bookingsCollection.find(query).toArray();
+
+            if (alreadyBooked.length) {
+                const message = `You already have a booking on : ${booking.appointmentDate}`
+                return res.send({ acknowledged: false, message })
+            }
+
             const result = await bookingsCollection.insertOne(booking);
             res.send(result);
         });
